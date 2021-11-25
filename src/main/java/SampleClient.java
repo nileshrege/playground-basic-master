df49ae8c40c050b120693f44e72cf3cdd8fe410e
rege.nilesh <rege.nilesh@gmail.com>
2021-11-25 13:06:33 -0500

    Update SampleClient.java
    moved code

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -15,36 +15,18 @@ import java.util.function.Function;
 public class SampleClient {
 
     public static void main(String[] theArgs) {
-
         // Create a FHIR client
         FhirContext fhirContext = FhirContext.forR4();
         IGenericClient client = fhirContext.newRestfulGenericClient("http://hapi.fhir.org/baseR4");
         client.registerInterceptor(new LoggingInterceptor(false));
 
         // Search for Patient resources
-        Bundle bundle = client
+        Bundle response = client
                 .search()
                 .forResource("Patient")
                 .where(Patient.FAMILY.matches().value("SMITH"))
                 .returnBundle(Bundle.class)
                 .execute();
-
-        List<IBaseResource> patients = new ArrayList<>();
-        patients.addAll(BundleUtil.toListOfResources(fhirContext, bundle));
-
-        Function<Patient, String> formatter = p -> "First Name: "
-                .concat(String.valueOf(p.getName().get(0).getGiven().get(0)))
-                .concat(" Last Name: ")
-                .concat(String.valueOf(p.getName().get(0).getFamily()))
-                .concat(" DOB: ")
-                .concat(String.valueOf(p.getBirthDate()));
-
-        patients.stream()
-                .filter(obj -> obj instanceof Patient)
-                .map(Patient.class::cast)
-                .sorted(Comparator.comparing(p -> String.valueOf(p.getName().get(0).getGiven().get(0))))
-                .forEach(p -> System.out.println(formatter.apply(p)));
-
     }
 
 }
